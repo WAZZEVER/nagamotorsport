32224aa15978b723a8b02e2ee4799ce214c2074c
WALTERB <101801884+WAZZEVER@users.noreply.github.com>
2026-05-28 16:30:37 +0930

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-// Resolve __filename and __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define routes for different HTML files
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Nagamotorsport running at http://localhost:${PORT}`);
 });
 
 export default app;
